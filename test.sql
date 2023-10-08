8e2cff8e211c7b55357adcfffda18978174ce015
Ankit <129873373+fnuankit164@users.noreply.github.com>
2023-10-07 23:54:23 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -6,3 +6,138 @@ Delete
 FROM VEGETABLE_DETAILS 
 where plant_name='Spinach' and root_depth_code='D';
 
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select grader(step, (actual = expected), actual, expected, description) as graded_results from 
+  ( SELECT 
+  'DORA_IS_WORKING' as step
+ ,(select 223) as actual
+ , 223 as expected
+ ,'Dora is working!' as description
+); 
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DABW01' as step
+ ,( select count(*) 
+   from PC_RIVERY_DB.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name ='PUBLIC') as actual
+ , 1 as expected
+ ,'Rivery is set up' as description
+);
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DABW02' as step
+ ,( select count(*) 
+   from PC_RIVERY_DB.INFORMATION_SCHEMA.TABLES 
+   where ((table_name ilike '%FORM%') 
+   and (table_name ilike '%RESULT%'))) as actual
+ , 1 as expected
+ ,'Rivery form results table is set up' as description
+);
+Delete
+--select (round(nutritions_sugar)) ,nutritions_sugar,*
+   from PC_RIVERY_DB.PUBLIC.FRUITYVICE
+  where nutritions_sugar not in (8.2,4.4,6,17.2);
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DABW03' as step
+ ,( select sum(round(nutritions_sugar)) 
+   from PC_RIVERY_DB.PUBLIC.FRUITYVICE) as actual
+ , 35 as expected
+ ,'Fruityvice table is perfectly loaded' as description
+);
+
+Set mystery_bag='What is in here?';
+Select $mystery_bag;
+
+set var1=1;
+set var2=2;
+set var3=3;
+Select $var1+$var2+$var3;
+
+create function sum_mystery_bag_vars(var1 number, var2 number, var3 number)
+returns number as 'Select var1+var2+var3';
+
+Select sum_mystery_bag_vars(12,36,204);
+
+-- Set these local variables according to the instructions
+set this = -10.5;
+set that = 2;
+set the_other =  1000;
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DABW04' as step
+ ,( select util_db.public.sum_mystery_bag_vars($this,$that,$the_other)) as actual
+ , 991.5 as expected
+ ,'Mystery Bag Function Output' as description
+);
+
+set alternating_caps_phrase = 'aLteRnatinG CapS!';
+Select $alternating_caps_phrase;
+
+Select initcap($alternating_caps_phrase);
+
+create function neutralize_whining(str1 text)
+returns text as 'Select initcap(str1)';
+
+Select neutralize_whining($alternating_caps_phrase);
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DABW05' as step
+ ,( select hash(neutralize_whining('bUt mOm i wAsHeD tHe dIsHes yEsTeRdAy'))) as actual
+ , -4759027801154767056 as expected
+ ,'WHINGE UDF Works' as description
+);
+
+show stages in ACCOUNT;
+
+list @my_internal_named_stage;
+
+select $1 from @my_internal_named_stage/sf_test1.txt.gz;
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DABW06' as step
+ ,( select count(distinct METADATA$FILENAME) 
+   from @util_db.public.my_internal_named_stage) as actual
+ , 3 as expected
+ ,'I PUT 3 files!' as description
+);
+
+use role pc_rivery_role;
+use warehouse pc_rivery_wh;
+
+create or replace TABLE PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST (
+	FRUIT_NAME VARCHAR(25)
+);
+
+truncate table PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST;
+
+insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+values ('banana')
+, ('cherry')
+, ('strawberry')
+, ('pineapple')
+, ('apple')
+, ('mango')
+, ('coconut')
+, ('plum')
+, ('avocado')
+, ('starfruit');
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+   SELECT 'DABW07' as step 
+   ,( select count(*) 
+     from pc_rivery_db.public.fruit_load_list 
+     where fruit_name in ('jackfruit','papaya', 'kiwi', 'test', 'from streamlit', 'guava')) as actual 
+   , 4 as expected 
+   ,'Followed challenge lab directions' as description
+); 
+
+
+
